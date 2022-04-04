8241a2deaedcb30619de1990b0a0938081368a5d
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2022-04-05 01:41:33 +0300

    let used instead of var or const

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
-  for (const idx of array) {
+  for (let idx of array) {
     newArray.push(`${appendString}${idx}`);
   }
 
