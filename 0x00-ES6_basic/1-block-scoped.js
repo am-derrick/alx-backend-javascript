8dce7c65fc595665f3bb6b275b4bf74423fe17e4
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2022-04-05 01:11:52 +0300

    indentation changed

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
-export defualt function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
 
-    if (trueOrFalse) {
-	var task = true;
-	var task2 = false;
-    }
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
 
-    return [task, task2];
+  return [task, task2];
 }
