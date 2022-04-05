5fe82f99fa155a3c52e5419ebdea3bfc3286a155
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2022-04-05 21:36:11 +0300

    Indent fixed

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,22 +2,22 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    const promises = [signUpUser(fileName, lastName),
-		      uploadPhoto(fileName)];
+  const promises = [signUpUser(firstName, lastName),
+    uploadPhoto(fileName)];
 
-    return Promise.allSettled(promises)
-	.then((values) => {
-	    const res = [];
-	    values.forEach((value) => {
-		if (value.status === 'fulfilled') {
-		    res.push(value);
-		} else {
-		    res.push({
-			status: value.status,
-			vallue: `Error: ${value.reason.message}`,
-		    });
-		}
-	    });
-	    return res;
-	});
+  return Promise.allSettled(promises)
+    .then((values) => {
+      const res = [];
+      values.forEach((value) => {
+        if (value.status === 'fulfilled') {
+          res.push(value);
+        } else {
+          res.push({
+            status: value.status,
+            value: `Error: ${value.reason.message}`,
+          });
+        }
+      });
+      return res;
+    });
 }
