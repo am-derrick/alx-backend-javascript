53f305f3d2ac97dfe110bd7d16771d563d377c1b
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2022-04-12 21:48:41 +0300

    view type correction made

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
-    if (position < 0 || position >= lenght) {
-	throw Error('Position outside range');
-    }
-    const buffer = new ArrayBuffer(length);
-    const int*view = new Int*Array(buffer, o, length);
-    int8View.set([value], position);
-    return new DataView(buffer);
+  if (position < 0 || position >= length) {
+    throw Error('Position outside range');
+  }
+  const buffer = new ArrayBuffer(length);
+  const int8View = new Int8Array(buffer, 0, length);
+  int8View.set([value], position);
+  return new DataView(buffer);
 }
