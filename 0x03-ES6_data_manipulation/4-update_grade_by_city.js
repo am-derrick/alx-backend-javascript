c39fa4c5b521915268d0901e468c08fdef38b1e9
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2022-04-12 21:47:52 +0300

    Extra curly bracket removed and indentation fixed

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,19 +1,17 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-    if (!Array.isArray(students)) {
-	return [];
-    }
-    if (!Array.isArray(newGrades)) {
-	return [];
-    }
-    if (Array.isArray(students)) {
-	return students
-	    .filter((student) => student.location === city)
-	    .map((student) => {
-		const[newGrade] = newGrade.filter((item) => item.studentId === student.id);
-		return {
-		    ...student,
-		    grade: newGrade ? newGrade.grade: 'N/A',
-		};
-	    });
-    }
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  if (!Array.isArray(newGrades)) {
+    return [];
+  }
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const [newGrade] = newGrades.filter((item) => item.studentId === student.id);
+      return {
+        ...student,
+        grade: newGrade ? newGrade.grade : 'N/A',
+      };
+    });
 }
