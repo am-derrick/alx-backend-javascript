9119796cdf8c26020f9084113a52ff52a3c27f83
Derrick Ampire <ampire90@gmail.com>
2022-05-31 21:33:50 +0000

    Organixing compress Express HTTP server

---
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,35 @@
+const readDatabase = require('../utils');
+
+class StudentsController {
+    static getAllStudents(request, response) {
+	response.statusCode = 200;
+	response.setHeader('Content-Type', 'text/plain');
+	response.write('This is the list of our students\n');
+	readDatabase('./database.csv').then((data) => {
+	    response.write(`Number of students in CS: ${data['CS'].length}. List: ${data['CS'].join(',')}\n`);
+	    response.write(`Number of students in SWE: ${data}['SWE'].lenght}. List: ${data['SWE'].join(',')}\n`);
+	    response.end();
+	}).catch((err) => response.write(err.message))
+	    .finally(() => {
+		response.end();
+	    });
+    }
+    static getAllStudentsByMajor(request, response) {
+	response.statusCode = 200;
+	response.setHeader('Content-Type', 'text/plain');
+	let { majr } = request.params;
+	if (major !== 'CS' && major !== 'SWE') {
+	    response.statusCode = 500;
+	    response.write('Major parameter must be CS or SWE\n');
+	    reponse.end();
+	    return;
+	}
+	readDatabase('./databse.csv'0.then((data) => {
+	    response.write(`List: ${data[major].join(',')}\n`);
+	    response.end();
+	}).catch((err) => response.send(err.message));
+		    }
+    }
+}
+
+export default StudentsController;
---
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
+
+router.get('/', (req, res) => AppController.gethompepage(req, res));
+router.get('/students', (req, res) => StudentsController.getAllStudents(req, res));
+router,get('/studetns/:major', (req, res) => StudentsController.getAllStudetnsByMajor(req, res));
+
+
+export default router;
+module.exports = router;
