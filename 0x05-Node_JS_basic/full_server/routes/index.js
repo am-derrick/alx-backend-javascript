1812749bfe2b2c0054dbfb054856f82975f5603e
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2022-06-01 00:38:34 +0300

    route fixed

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -6,7 +6,7 @@ import StudentsController from '../controllers/StudentsController';
 
 router.get('/', (req, res) => AppController.gethompepage(req, res));
 router.get('/students', (req, res) => StudentsController.getAllStudents(req, res));
-router,get('/studetns/:major', (req, res) => StudentsController.getAllStudetnsByMajor(req, res));
+router.get('/students/:major', (req, res) => StudentsController.getAllStudetnsByMajor(req, res));
 
 
 export default router;
