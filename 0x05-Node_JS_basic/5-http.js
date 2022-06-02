ca728703fe9ea49e475bd750bfa124c68530be50
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2022-06-02 14:06:03 +0300

    Update 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -20,7 +20,7 @@ const app = http.createServer((req, res) => {
 });
 
 app.listen(port, hostname, () => {
-    console.log(`Server running at http://${port}`);
+    console.log(`Server running at http://${hostname}:${port}`);
 });
 
 module.export = app;
